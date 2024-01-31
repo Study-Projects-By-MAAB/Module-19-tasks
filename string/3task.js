f0d5dcb4083f0232c5abab7bacf63c9ec7845989
maabfw <maabfw@proton.me>
2024-01-31 21:48:06 +0600

    string task 3 done

---
+++ string/3task.js
@@ -0,0 +1,10 @@
+// Check whether a string contains all the vowels a, e, i, o, u
+
+let string = "Check whether a string contains all the vowels a, e, i, o, u"
+
+if (string.includes('a' && 'e' && 'i' && 'o' && 'u')) {
+    console.log("yes it's have");
+}
+else {
+    console.log("no it's not!");
+}
